ce52573e66623ac93bf4cc7f7be87854fd5ad168
soumitradev <soumitra.shewale.dev@gmail.com>
2023-01-07 21:45:29 +0530

    Add zod validation to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 // @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
 import NextBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import("next").NextConfig} */
