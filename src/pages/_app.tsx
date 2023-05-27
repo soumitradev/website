5ceaee0c82e30e9b9c960ee8f1ca33539e210723
soumitradev <soumitra.shewale.dev@gmail.com>
2023-05-27 12:26:01 +0530

    feat: Use fonts defined by tailwind in frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto_Flex, JetBrains_Mono } from "next/font/google";
+
+// const customMaterialIconFont = localFont({
+//   src: "../styles/CustomMaterialIcons.woff2",
+//   variable: "--font-custom-material-icons",
+//   display: "swap",
+// });
+
+const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+  variable: "--font-roboto-flex",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${robotoFlex.variable} ${jetbrainsMono.variable} font-sans`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
