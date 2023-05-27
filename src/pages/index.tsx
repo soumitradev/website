7dffb1f073c47b474feef904a9738f378043175f
soumitradev <soumitra.shewale.dev@gmail.com>
2023-05-27 12:33:18 +0530

    style: Format document and index tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col justify-between items-center min-h-screen"></main>
+      <main className="bg-cr flex min-h-screen flex-col items-center justify-between"></main>
     </>
   );
 }
